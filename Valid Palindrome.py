9ae5e301e64481b735c797e93cb21eb040504ca7
Suryadeep kumar Tiwari <surya99.jin@gmail.com>
2021-07-04 19:58:23 +0530

    Add files via upload

---
+++ Valid Palindrome.py
@@ -0,0 +1,29 @@
+'''Valid Palindrome
+Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+ 
+
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+'''
+
+##### My Solution AKA Best Solution #####
+import re
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        str_ = re.sub('\W+|_','', s.lower())
+        return str_==str_[::-1]
